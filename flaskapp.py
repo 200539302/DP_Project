4231234e9349fc27f139ff6e8ca352cdfae27ad2
200539302 <137479838+200539302@users.noreply.github.com>
2023-08-17 02:48:36 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -90,4 +90,4 @@ batch_thread = threading.Thread(target=fetch_and_store_data)
 
 if __name__ == '__main__':
     batch_thread.start()  # Start the batch process thread
-    app.run()   # Start the Flask app
+    app.run(host='0.0.0.0', port=80)
